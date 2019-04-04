87e37a45261343cc7db8260c805c305665728fb1
Ivan Belov <kaiepolar@gmail.com>
2019-04-04 03:35:51 +0300

    04.04.2019 Timer widget added

--- app/src/main/java/com/a/clock/Views/Fragments/TimerFragment.java
+++ app/src/main/java/com/a/clock/Views/Fragments/TimerFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.a.clock.Presenters.TimerPresenter;
@@ -19,9 +20,12 @@ import java.util.List;
 
 public class TimerFragment extends Fragment implements com.a.clock.Interfaces.View {
 
-    private TextView timerTextView;
     private Button timerButton;
     private TimerPresenter presenter;
+    private ImageView hoursUpImageView, minutesUpImageView, secondsUpImageView;
+    private ImageView hoursDownImageView, minutesDownImageView, secondsDownImageView;
+    private TextView hoursTextView, minutesTextView, secondsTextView;
+    private String hours = "00", minutes = "00", seconds = "00";
 
 
     @Override
@@ -36,15 +40,146 @@ public class TimerFragment extends Fragment implements com.a.clock.Interfaces.Vi
                              Bundle savedInstanceState) {
         presenter.bindView(this);
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
-
-
         timerButton = view.findViewById(R.id.timer_start_button);
-        timerTextView = view.findViewById(R.id.timer_text_view);
+        hoursDownImageView = view.findViewById(R.id.hours_down_image_view);
+        minutesDownImageView = view.findViewById(R.id.minutes_down_image_view);
+        secondsDownImageView = view.findViewById(R.id.seconds_down_image_view);
+        hoursUpImageView = view.findViewById(R.id.hours_up_image_view);
+        minutesUpImageView = view.findViewById(R.id.minutes_up_image_view);
+        secondsUpImageView = view.findViewById(R.id.seconds_up_image_view);
+        hoursTextView = view.findViewById(R.id.hours_digits_text_view);
+        minutesTextView = view.findViewById(R.id.minutes_digits_text_view);
+        secondsTextView = view.findViewById(R.id.seconds_digits_text_view);
+
+        hoursUpImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hoursInt = Integer.parseInt(hoursTextView.getText().toString());
+                if (hoursInt != 99) {
+                    ++hoursInt;
+                }
+                if (hoursInt < 10) {
+                    hours = "0" + hoursInt;
+                }
+                if (hoursInt >= 10) {
+                    hours = String.valueOf(hoursInt);
+                }
+                hoursTextView.setText(hours);
+            }
+        });
+        minutesUpImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int minutesInt = Integer.parseInt(minutesTextView.getText().toString());
+                if (minutesInt != 59) {
+                    ++minutesInt;
+                }
+                if (minutesInt < 10) {
+                    minutes = "0" + minutesInt;
+                }
+                if (minutesInt >= 10) {
+                    minutes = String.valueOf(minutesInt);
+                }
+                minutesTextView.setText(minutes);
+            }
+        });
+        secondsUpImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int secondsInt = Integer.parseInt(secondsTextView.getText().toString());
+                if (secondsInt != 59) {
+                    ++secondsInt;
+                }
+                if (secondsInt < 10) {
+                    seconds = "0" + secondsInt;
+                }
+                if (secondsInt >= 10) {
+                    seconds = String.valueOf(secondsInt);
+                }
+                secondsTextView.setText(seconds);
+            }
+        });
+
+        hoursDownImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hoursInt = Integer.parseInt(hoursTextView.getText().toString());
+                if (hoursInt != 0) {
+                    --hoursInt;
+                }
+                if (hoursInt < 10) {
+                    hours = "0" + hoursInt;
+                }
+                if (hoursInt >= 10) {
+                    hours = String.valueOf(hoursInt);
+                }
+                hoursTextView.setText(hours);
+            }
+        });
+        minutesDownImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int minutesInt = Integer.parseInt(minutesTextView.getText().toString());
+                if (minutesInt != 0) {
+                    --minutesInt;
+                }
+                if (minutesInt < 10) {
+                    minutes = "0" + minutesInt;
+                }
+                if (minutesInt >= 10) {
+                    minutes = String.valueOf(minutesInt);
+                }
+                minutesTextView.setText(minutes);
+            }
+        });
+        secondsDownImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int secondsInt = Integer.parseInt(secondsTextView.getText().toString());
+                if (secondsInt != 0) {
+                    --secondsInt;
+                }
+                if (secondsInt < 10) {
+                    seconds = "0" + secondsInt;
+                }
+                if (secondsInt >= 10) {
+                    seconds = String.valueOf(secondsInt);
+                }
+                secondsTextView.setText(seconds);
+            }
+        });
+
+//        timerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                long time = ((Integer.parseInt(hoursTextView.getText().toString()))*360+(Long.parseLong(minutesTextView.getText().toString()))*60+(Long.parseLong(secondsTextView.toString()));
+//                Toast.makeText(getContext(),String.valueOf(time),Toast.LENGTH_SHORT);
+//                final CountDownTimer timer = new CountDownTimer(time,1000) {
+//                    @Override
+//                    public void onTick(long millisUntilFinished) {
+//                        int secondsInt = Integer.parseInt(secondsTextView.getText().toString());
+//                        if (secondsInt != 0) {
+//                            --secondsInt;
+//                        }
+//                        if (secondsInt < 10) {
+//                            seconds = "0" + secondsInt;
+//                        }
+//                        if (secondsInt >= 10) {
+//                            seconds = String.valueOf(secondsInt);
+//                        }
+//                        secondsTextView.setText(seconds);
+//                    }
+//
+//                    @Override
+//                    public void onFinish() {
+//
+//                    }
+//                };
+//            }
+//        });
 
         return view;
     }
-
-
 
 
     @Override
